b7188de113172ba7ec6fede2967f24a3e0888de1
mayan atid <atid_m@qwasar.io>
2022-10-31 21:49:56 +0000

    writing tests for file info

---
+++ my_tar.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+
+// Thoughts
+// the tar command takes a file (or files) and compresses it into a .tar format.
---
+++ tarTest/file_info_tests.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <grp.h>
+#include <pwd.h>
+
+int main()
+{
+    struct stat st;
+    struct group *grp;
+    struct passwd *pwd;
+
+    stat("test.txt", &st);
+    printf("group ID: %o\n", st.st_gid);
+    printf("user ID: %o\n", st.st_uid);
+    printf("mod time: %o\n", (int)st.st_mtim.tv_sec);
+    printf("size %o\n", (int)st.st_size);
+    grp = getgrgid(st.st_gid);
+    printf("group name %s\n", grp->gr_name);
+    pwd = getpwuid(st.st_uid);
+    printf("user name %s\n", pwd->pw_name);
+
+     
+
+    return 0;
+}
